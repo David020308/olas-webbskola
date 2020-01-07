6333cd95564200098e2ea31f60a4eee0e271039d
Ola Lindberg <olalindberg.com>
2020-01-07 15:35:21 +0100

    Added simple mini hamburger menu

---
+++ HTML-CSS/menu/hamburger.js
@@ -0,0 +1,21 @@
+document.getElementById('menu-button').addEventListener('click', function () {
+    toggleClass(document.getElementById('main-menu'), "active")
+});
+
+/**
+ * Function to toggle the given class (className) on the given element (element)
+ * From: http://jsfiddle.net/4MBQa/
+*/
+function toggleClass(element, className) {
+    if (!element || !className) {
+        return;
+    }
+
+    var classString = element.className, nameIndex = classString.indexOf(className);
+    if (nameIndex == -1) {
+        classString += ' ' + className;
+    } else {
+        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+    }
+    element.className = classString;
+}
