642de473305dc7807bd25998035675ecad874520
Ola Lindberg <olalindberg.com>
2020-01-08 11:04:55 +0100

    Using querySelector instead of getElementById

--- HTML-CSS/menu/hamburger.js
+++ HTML-CSS/menu/hamburger.js
@@ -1,3 +1,3 @@
-document.getElementById('menu-button').addEventListener('click', function () {
+document.querySelector('#menu-button').addEventListener('click', function () {
     document.querySelector('#main-menu').classList.toggle('active');
 });
